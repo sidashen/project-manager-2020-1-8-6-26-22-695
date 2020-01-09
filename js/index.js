45b050e397f7248be85431470e5d6bf87ed156fe
sidashen <shashasi8023@163.com>
2020-01-10 01:04:10 +0800

    [shasha] feat: delete function

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 const API_ROOT = 'http://localhost:3000/projects';
 let projectList = document.getElementById('project-lists-main');
+let id;
 
 (function handleProductList() {
-  getListData();
+  getProjectListData();
 })();
 
-function getListData() {
+function getProjectListData() {
   $.ajax({
     url: API_ROOT,
     type: 'get',
@@ -18,10 +19,26 @@ function getListData() {
   });
 }
 
+function deleteProjectData(id, event) {
+  $.ajax({
+    url: `${API_ROOT}/${id}`,
+    type: 'delete',
+    crossDomain: true,
+    success: function(res) {
+      deleteProject(id, event);
+      const projectStatus = document.getElementsByClassName('single-status');
+      setData(projectStatus);
+    },  // 请求成功后调用此方法
+    // fail: function(error) {
+    //   console.log('请求失败了哟');
+    // }    // 请求失败或出错后调用此方法
+  });
+}
+
 function projectListData(res) {
   let list = '';
   res.forEach(item => {
-    list += `<tr><td>${item.name}</td><td><div id='description'>${item.description}</div></td>
+    list += `<tr id='${item.id}'><td>${item.name}</td><td><div id='description'>${item.description}</div></td>
     <td>${item.endTime}</td><td class='single-status'>${item.status}</td>
     <td><button class='btn'>删除</button></td></tr>`;
   });
@@ -62,28 +79,38 @@ function setData(projectStatus) {
   $('.closed-percent').html(`${closedPercent}%`);
 }
 
-function confirm() {
+function confirm(event) {
   let doc = document.documentElement;
   relHeight = (doc.clientHeight > doc.scrollHeight) ? doc.clientHeight : doc.scrollHeight;
   $('#mask').height(`${relHeight}px`);
   $('#mask').css('display','flex');
+  id = event.target.parentNode.parentNode.getAttribute('id');
 }
 
+function deleteProject(id) {
+  projectList.removeChild(document.getElementById(id));
+  $('#mask').css('display','none');
+  // let item = $userList.querySelector(`li[data-id='${id}']`);
+  // $userList.removeChild(item);
+}
 
-
-$('body').click(e => {
-  const target = e.target.innerHTML;
-  if (target === "删除") {
-    confirm();
+$('body').click(event => {
+  const target = event.target.innerHTML;
+  if (target === '删除') {
+    confirm(event);
+  };
+  if (target === '取消') {
+    event.target.parentNode.parentNode.style.display = 'none';
   }
-  if (target === "取消") {
-    e.target.parentNode.parentNode.style.display = 'none';
+  if (target === '确认') {  
+    deleteProjectData(id);
   }
 });
 
-$('.icon-guanbi').click(e => {
-  e.target.parentNode.parentNode.style.display = 'none';
+$('.icon-guanbi').click(event => {
+  event.target.parentNode.parentNode.style.display = 'none';
 });
+
 
 
         
