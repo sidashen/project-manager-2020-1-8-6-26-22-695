96bef7d493d1f19718dfa7d597209053ce5b8293
sidashen <shashasi8023@163.com>
2020-01-09 16:04:12 +0800

    [shasha]:set project lists style

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const API_ROOT = 'http://localhost:3000/projects';
 let projectList = document.getElementById('project-lists-main');
 
-(function handleProductList() {
+(function handleProductList(projectStatus) {
   getListData();
 })();
 
@@ -13,6 +13,8 @@ function getListData() {
     crossDomain: true,
     success: function (res) {
       projectListData(res);
+      const projectStatus = document.getElementsByClassName('single-status');
+      setFontColor(projectStatus);
     },
   });
 }
@@ -20,11 +22,25 @@ function getListData() {
 function projectListData(res) {
   let list = '';
   res.forEach(item => {
-    list += `<tr><td>${item.name}</td><td><div class='description'>${item.description}</div></td>
-    <td>${item.endTime}</td><td>${item.status}</td>
+    list += `<tr><td>${item.name}</td><td><div id='description'>${item.description}</div></td>
+    <td>${item.endTime}</td><td class='single-status'>${item.status}</td>
     <td><button class='btn'>删除</button></td></tr>`;
   });
   projectList.innerHTML = list;
 }
+
+function setFontColor(projectStatus) {
+  projectStatusArr = Array.from(projectStatus);
+  projectStatusArr.map(item => {
+    if (item.innerHTML === 'ACTIVE') {
+      item.style.color = '#666666';
+    } else if (item.innerHTML === 'PENDING') {
+      item.style.color = '#ee706d';
+    } else if (item.innerHTML === 'CLOSED') {
+      item.style.color = '#f7da47';
+    }
+  });
+}
+
 
 
